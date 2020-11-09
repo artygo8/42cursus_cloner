1e6dcd98c9d1dcbea5565aa079e7cbcaa2452659
Artygo8 <arthurgossuin@gmail.com>
2020-11-09 11:06:20 +0100

    echo to printf for colors

--- 42cursus.sh
+++ 42cursus.sh
@@ -34,22 +34,11 @@ fi
 
 CITY=${CITY^}
 
-# GET ACCESS TOKEN
-MY_AWESOME_SECRET=61685f90ae7ec137cb916dd785cf3d7252dbfd13007d21ead5ff9c150d72f4d5
-MY_AWESOME_UID=4c8b6090c10edd4d18bfe036d2ddaacffd63beb223edecdb761d7ebcf0ed7edd
-ACCESS_TOKEN=`curl -s -X POST --data "grant_type=client_credentials&client_id=$MY_AWESOME_UID&client_secret=$MY_AWESOME_SECRET" https://api.intra.42.fr/oauth/token | jq ".access_token"`
+ACCESS_TOKEN=`curl -s -X POST --data "grant_type=client_credentials&client_id=4c8b6090c10edd4d18bfe036d2ddaacffd63beb223edecdb761d7ebcf0ed7edd&client_secret=61685f90ae7ec137cb916dd785cf3d7252dbfd13007d21ead5ff9c150d72f4d5" https://api.intra.42.fr/oauth/token | jq ".access_token"`
 
-function echo_red() {
-  echo -e -n "\e[031m";echo $@;echo -e -n "\e[m"
-}
-
-function echo_green() {
-  echo -e -n "\e[032m";echo $@;echo -e -n "\e[m"
-}
-
-function echo_gold() {
-  echo -e -n "\e[033m";echo $@;echo -e -n "\e[m"
-}
+RED="\e[031m%s\e[m\n"
+GREEN="\e[032m%s\e[m\n"
+GOLD="\e[033m%s\e[m\n"
 
 function get_all() {
   file="srcs/${@//\//_}.json"
@@ -83,19 +72,19 @@ function get_all() {
 get_all campus
 campus_id=`cat srcs/campus.json | jq "select(.name==\"$CITY\") | .id"`
 if [ -z $campus_id ]; then
-  echo_red "Your campus ($CITY) was not found."; exit
+  printf $RED "Your campus ($CITY) was not found."; exit
 fi
 
-echo_green "city:  $CITY's id is $campus_id."
+printf $GREEN "city:  $CITY's id is $campus_id."
 
 # Retrieve login (can take some times...)
 get_all campus/${campus_id}/users
 user_id=`cat srcs/campus_${campus_id}_users.json | jq "select(.login==\"$LOGIN\") | .id"`
 if [ -z $campus_id ]; then
-  echo_red "Your login ($LOGIN) was not found in the $CITY's campus data".; exit
+  printf $RED "Your login ($LOGIN) was not found in the $CITY's campus data".; exit
 fi
 
-echo_green "login: $LOGIN's id is $user_id."
+printf $GREEN "login: $LOGIN's id is $user_id."
 
 get_all users/${user_id}/projects_users
 get_all users/${user_id}/scale_teams
@@ -110,10 +99,10 @@ function 42_cursus_cloner() {
     mkdir -p $dir
 
     repo=`cat srcs/users_${user_id}_projects_users.json | jq "select(.project.name==$line) | .teams | .[-1] | .repo_url"`
-    echo_gold Vogsphere ${line:1:(-1)}
+    printf $GOLD Vogsphere ${line:1:(-1)}
     git clone ${repo:1:(-1)} $dir/vogsphere-${line:1:(-1)}
     if [ ! -z $GITHUB_PREFIX ]; then
-      echo_gold Github ${line:1:(-1)}
+      printf $GOLD Github ${line:1:(-1)}
       git clone $GITHUB_PREFIX${line:1:(-1)} $dir/github-${line:1:(-1)}
     fi
   done
